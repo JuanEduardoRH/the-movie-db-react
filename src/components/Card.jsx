3fdd8042d0219c6a3c7600dbb41736cfc71ebe29
Juan Rios <juanrios.h@hotmail.com>
2023-07-02 21:36:31 -0400

    ajuste tailwind config

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ path, className = '', footer = null, onMouseEnter }) => {
     return (
         <div
             onMouseEnter={onMouseEnter}
-            className={`group/overlay rounded-lg overflow-hidden transition-all duration-300 hover:scale-110 hover:z-10 cursor-pointer shadow-lg shadow-black/25 relative ${className}`}>
+            className={`group/overlay rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:z-10 cursor-pointer shadow-lg shadow-black/25 relative ${className}`}>
             <img src={path} className='object-cover h-full w-full' alt='imagen'></img>
             <div className='absolute overflow-hidden flex flex-col-reverse flex-1 transition-all duration-300 inset-x-0 bottom-0 top-full group-hover/overlay:top-1/4 bg-gradient-to-t from-black/80 to-transparent'>
                 {footer}
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -23,15 +23,15 @@ export const Home = () => {
     const { isLoading } = useNowPlayingMovies(currentPage, setListMovies);
 
     return (
-        <div className='container mx-auto pt-2'>
+        <div className='container px-2 sm:px-4 mx-auto pt-2'>
             <div className='pb-5'>
-                <h2 className='text-primary text-4xl font-bold pb-5'>Busca tu pelicula</h2>
+                <h2 className='text-primary text-2xl sm:text-4xl font-bold pb-5'>Busca tu pelicula</h2>
                 <Search />
             </div>
 
             <div className='pb-5'>
-                <h2 className='text-primary text-4xl font-bold pb-10'>Peliculas recientes</h2>
-                <div className='card-container grid grid-cols-4 gap-8'>
+                <h2 className='text-primary text-2xl sm:text-4xl font-bold pb-10'>Peliculas recientes</h2>
+                <div className='card-container grid xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8'>
                     {isLoading && (<h1>Cargando...</h1>)}
                     {listMovies.map((movie) => <MovieCard key={movie.id} {...movie} />)}
                 </div>
