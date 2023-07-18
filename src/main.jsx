b1b99e32f3d3ced2c6a3f34183b4eb8a0df1cb42
Juan Rios <juanrios.h@hotmail.com>
2023-07-17 23:05:36 -0400

    404 page

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,8 @@ import "swiper/css/effect-fade";
 
 import 'react-loading-skeleton/dist/skeleton.css'
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
